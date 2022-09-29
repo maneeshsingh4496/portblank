66967bcefb35d9b4afbaa0462e4cb35bf42d8705
Manish Singh <maneeshsingh@gmail.com>
2022-09-29 22:27:17 +0530

    first

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Manish
+          Learn React Manish data
         </a>
       </header>
     </div>
